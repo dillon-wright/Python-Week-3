1b7977bfe0ecf3165d11d1c6aa1e0a3230a82e9f
Dillon Wright <dillon@wrightsolutions.tech>
2021-05-18 18:39:14 +1000

    test

---
+++ Class-2/Activities/01-Stu_QuickCheckup/Solved/quick_check_up.py
@@ -0,0 +1,16 @@
+# Print Hello User!
+print("Hello User")
+
+# Take in User Input
+x = input("Please inpuit")
+
+# Respond Back with User Input
+
+print(x)
+
+# Take in the User Age
+y = int(input("what is your age?"))
+
+# Respond Back with a statement based on age
+if y > 30:
+    print("Past your prime ay cobba?")
