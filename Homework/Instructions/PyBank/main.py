650713c51f96eda8298ab02597b26b9414dd39f6
Dillon Wright <dillon@wrightsolutions.tech>
2021-05-26 22:04:40 +1000

    Finshed the first homework for sure this time

--- Homework/Instructions/PyBank/main.py
+++ Homework/Instructions/PyBank/main.py
@@ -1,7 +1,8 @@
-import os
 import csv
 import numpy as np
+import sys
 
+# declare all of the important variables
 row_count = 0
 sum_total = 0
 mean_list = []
@@ -13,38 +14,55 @@ largest_loss = 0
 largest_l_date = ''
 largest_increase = 0
 largest_i_date = ''
+date_list = []
+Head_0 = ''
 
-
+# Open the CSV File and iterate through all rows
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader,None)
+
+    next(csvreader, None)
 
     for row in csvreader:
         sum_total = sum_total + int(row[1])
         mean_list.append(int(row[1]))
+        date_list.append(str(row[0]))
         row_count += 1
-        if largest_increase < int(row[1]):
-            largest_i_date = row[0]
-        if largest_loss > int(row[1]):
-            largest_l_date = row[0]
-        #print(row)
-        #print(np.mean(mean_list))
 
+# Iterate though the list of all values and determien the change
 
 for counter in mean_list:
     cur_value = counter
     if last_value != 0:
         change_list.append((cur_value - last_value))
     last_value = counter
-    print(counter)
+
+# Set the final vars via an index or min / max
 
 largest_increase = max(change_list)
 largest_loss = min(change_list)
+largest_i_date = date_list[change_list.index(largest_increase) + 1]
+largest_l_date = date_list[change_list.index(largest_loss) + 1]
+
+# print to console
 
 print('Financial Analysis')
 print('----------------------------')
 print('Total Months: ' + str(row_count))
 print('Total: $' + str(sum_total))
-print('Average  Change: $'+str(round(np.mean(change_list), 2)))
-print('Greatest Increase in Profits: '+largest_i_date+' ($'+str(largest_increase)+')')
-print('Greatest Decrease in Profits: '+largest_l_date+' ($'+str(largest_loss)+')')
+print('Average  Change: $' + str(round(np.mean(change_list), 2)))
+print('Greatest Increase in Profits: ' + largest_i_date + ' ($' + str(largest_increase) + ')')
+print('Greatest Decrease in Profits: ' + largest_l_date + ' ($' + str(largest_loss) + ')')
+
+# Print to file
+
+Analysis_File = open('S:/Bootcamp/Python-Week-3/Homework/Instructions/PyBank/Analysis/Analysis_File.txt', 'w')
+sys.stdout = Analysis_File
+print('Financial Analysis')
+print('----------------------------')
+print('Total Months: ' + str(row_count))
+print('Total: $' + str(sum_total))
+print('Average  Change: $' + str(round(np.mean(change_list), 2)))
+print('Greatest Increase in Profits: ' + largest_i_date + ' ($' + str(largest_increase) + ')')
+print('Greatest Decrease in Profits: ' + largest_l_date + ' ($' + str(largest_loss) + ')')
+Analysis_File.close()
